2a64a9bca797ae7aef8a083bd81cc9fe7006b048
Rodney <rgarlandjr23@gmail.com>
2020-07-21 09:33:34 -0500

    First Commit

---
+++ Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using RPG.Core;
+
+namespace RPG.Movement
+{
+//change this to MoverPlayer1
+    public class Mover : MonoBehaviour, IAction
+    {
+        [SerializeField] Transform target;
+
+        NavMeshAgent navMeshAgent;
+
+        public void Start()
+        {
+            navMeshAgent = GetComponent<NavMeshAgent>();
+
+        }
+        
+        void Update()
+        {
+            UpdateAnimator();
+
+        }
+
+        public void StartMoveAction(Vector3 destination)
+        {
+            GetComponent<ActionScheduler>().StartAction(this);
+            MoveTo(destination);
+
+        }
+
+        public void MoveTo(Vector3 destination)
+        {
+            navMeshAgent.destination = destination;
+            navMeshAgent.isStopped = false;
+
+        }
+
+        public void Cancel()
+        {
+            navMeshAgent.isStopped = true;
+
+        }
+
+        private void UpdateAnimator()
+        {
+            Vector3 velocity = navMeshAgent.velocity;
+            //Allows us to convert to a local velocity 
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+            float speed = localVelocity.z;
+            GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+
+        }
+    }
+}
